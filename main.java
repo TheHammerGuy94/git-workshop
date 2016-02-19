22358894cfe2fc71318c12ce3d268f4322acfcdb
TheHammerGuy94 <thesnipierrerifle@gmail.com>
2016-02-19 15:29:35 +0800

    Added files via upload
    made by Pierre

---
+++ main.java
@@ -0,0 +1,14 @@
+package tutorial;
+
+public class main {
+
+	public main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		
+	}
+
+}
